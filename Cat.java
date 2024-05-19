c5872cfd290e457cc0b0aedf9f576b3a14a00a89
Devin Brown <102552455+DevinBrown11@users.noreply.github.com>
2024-05-18 23:04:32 -0500

    Update Cat.java
    This file stores the information of the fields to call and a Meow Method that is supposed to be called from the "JavaProgram.java" file to be printed.

--- Cat.java
+++ Cat.java
@@ -1,10 +1,10 @@
+//This file is where the information of the fields and Meow method is being stored. 
+//Numbers 6 and 7 should utilize this file to run the program.
+
 public class Cat {
   String name;
   int age;
   public static void meow() {
     System.out.println("Meow");
   }
-
-
-
 }
